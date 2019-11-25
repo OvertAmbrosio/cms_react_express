a1d80685936ba17cc5c947d9c2bfd4bb1112cf7f
Your NameOvert <zero_uno@hotmail.com>
2019-11-24 23:31:03 -0500

    24-11
    Signed-off-by: Your NameOvert <zero_uno@hotmail.com>

--- backend_express_v1.2/app/global.js
+++ backend_express_v1.2/app/global.js
@@ -1,6 +1,6 @@
 const ReactApi = {
-  url_api_dev: "http://34.239.113.20:4000",
-  url_api: "http://localhost:4000"
+  url_api: "http://34.239.113.20:4000",
+  url_api_dev: "http://localhost:4000"
 }
 
 export default ReactApi;
